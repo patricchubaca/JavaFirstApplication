dc661cd367fd9f9236f74f03eafb1786c989da85
patricchubaca <patric_job@outlook.com>
2025-04-15 13:44:17 -0300

    First :: Java APP

--- src/App.java
+++ src/App.java
@@ -1,11 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
+
         int numero1, numero2, soma;
 
+        float floatNum = 1.8f;
+
         numero1 = 1;
+
         numero2 = 2;
+
         soma = numero1 + numero2;
 
         System.out.println(soma);
+
+        System.out.println(floatNum + numero1);
     }
 }
